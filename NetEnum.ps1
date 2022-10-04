8902e08a6003cbe27e9dec271538edc66a50cc3b
Liam Powell <Powell.liam.p@gmail.com>
2022-10-04 05:40:37 -0500

    Update NetEnum.ps1
    Added search adapter via remote connection. Added process ID to active connections. Updated help menu.

--- NetEnum.ps1
+++ NetEnum.ps1
@@ -22,79 +22,104 @@ function Gather{
         $Adapter = Get-CimInstance Win32_NetworkAdapterConfiguration 
     }
     foreach($x in $Adapter){
-        if($Out){
-            Write-Host "Writing to $out..." -ForegroundColor $GoodColor
-            echo "<-----$x----->" >> $Out
-        }else{
-            Write-Host "<-----$x----->" -ForegroundColor $GoodColor
-        }
-        if($Out){
-            ft -InputObject $x -Property Description, IPAddress, IPSubnet, DefaultIPGateway, DNSHostName, DNSServerSearchOrder -AutoSize >> $Out
-            }else{
-                ft -InputObject $x -Property Description, IPAddress, IPSubnet, DefaultIPGateway, DNSHostName, DNSServerSearchOrder -AutoSize
-            }
-        if($Out){
-            Write-Host "Writing Connections"
-            echo "<-----Connections----->" >> $Out
-        }else{
-            Write-Host "<-----Connections----->"
-        }
-        try{
-            $Connections = Get-NetTCPConnection -LocalAddress $x.IPAddress[0]
+        if(!$Conn){
             if($Out){
-                ft -InputObject $Connections -Property LocalAddress, LocalPort, RemoteAddress, RemotePort -AutoSize >> $Out
+                Write-Host "Writing to $out..." -ForegroundColor $GoodColor
+                echo "<-----$x----->" >> $Out
+            }else{
+                Write-Host "<-----$x----->" -ForegroundColor $GoodColor
+            }
+            if($Out){
+                ft -InputObject $x -Property Description, IPAddress, IPSubnet, DefaultIPGateway, DNSHostName, DNSServerSearchOrder -AutoSize >> $Out
                 }else{
-                    ft -InputObject $Connections -Property LocalAddress, LocalPort, RemoteAddress, RemotePort -AutoSize
+                    ft -InputObject $x -Property Description, IPAddress, IPSubnet, DefaultIPGateway, DNSHostName, DNSServerSearchOrder -AutoSize
                 }
-            if($Verbose){
-                try{
-                    $DNSList=@()
-                    if($Out){
-                        Write-Host "Writing DNS Names"
-                        echo " <-----Resolving DNS Names----->" >> $Out
+            if($Out){
+                Write-Host "Writing Connections"
+                echo "<-----Connections----->" >> $Out
+            }else{
+                Write-Host "<-----Connections----->"
+            }
+            try{
+                $Connections = Get-NetTCPConnection -LocalAddress $x.IPAddress[0]
+                if($Out){
+                    ft -InputObject $Connections -Property LocalAddress, LocalPort, RemoteAddress, RemotePort, OwningProcess -AutoSize >> $Out
                     }else{
-                        Write-Host "<-----Resolving DNS Names----->"
+                        ft -InputObject $Connections -Property LocalAddress, LocalPort, RemoteAddress, RemotePort, OwningProcess -AutoSize
                     }
-                    foreach($x in $connections){
-                        $IsDNS = Resolve-DNSName $x.RemoteAddress
-                        if($IsDNS){
-                            $DNSList += $IsDNS
-                        }
-                    }
-                    if($Out){
-                        ft -InputObject $DNSList -Property Name, Server, Type >> $Out
+                if($Verbose){
+                    try{
+                        $DNSList=@()
+                        if($Out){
+                            Write-Host "Writing DNS Names"
+                            echo " <-----Resolving DNS Names----->" >> $Out
                         }else{
-                            ft -InputObject $DNSList -Property Name, Server, Type
+                            Write-Host "<-----Resolving DNS Names----->"
                         }
-                }catch{
-
-                }
-                try{
-                    $TestPort =@()
-                    if($Out){
-                        Write-Host "Writing Connection Test"
-                        echo "<-----Testing Connections----->" >> $Out
-                    }else{
-                        Write-Host "<-----Testing Connections----->"
-                    }
-                    foreach($x in $connections){
-                        $strx = (Out-String -InputObject $x.RemoteAddress).Trim()
-                        $TestPort += Get-CimInstance Win32_PingStatus -filter "Address='$strx' AND Timeout=1000"
-                    }
-                    if($Out){
-                        ft -InputObject $TestPort -Property Address, IPV4Address, IPV6Address, ResponseTime, StatusCode -AutoSize >> $Out
-                        Write-Host "Report generated!"
-                        }else{
-                            ft -InputObject $TestPort -Property Address, IPV4Address, IPV6Address, ResponseTime, StatusCode -AutoSize
+                        foreach($x in $connections){
+                            $IsDNS = Resolve-DNSName $x.RemoteAddress
+                            if($IsDNS){
+                                $DNSList += $IsDNS
+                            }
                         }
+                        if($Out){
+                            ft -InputObject $DNSList -Property Name, Server, Type >> $Out
+                            }else{
+                                ft -InputObject $DNSList -Property Name, Server, Type
+                            }
                     }catch{
 
                     }
+                    try{
+                        $TestPort =@()
+                        if($Out){
+                            Write-Host "Writing Connection Test"
+                            echo "<-----Testing Connections----->" >> $Out
+                        }else{
+                            Write-Host "<-----Testing Connections----->"
+                        }
+                        foreach($x in $connections){
+                            $strx = (Out-String -InputObject $x.RemoteAddress).Trim()
+                            $TestPort += Get-CimInstance Win32_PingStatus -filter "Address='$strx' AND Timeout=1000"
+                        }
+                        if($Out){
+                            ft -InputObject $TestPort -Property Address, IPV4Address, IPV6Address, ResponseTime, StatusCode -AutoSize >> $Out
+                            Write-Host "Report generated!"
+                            }else{
+                                ft -InputObject $TestPort -Property Address, IPV4Address, IPV6Address, ResponseTime, StatusCode -AutoSize
+                            }
+                        }catch{
+
+                        }
+                }
+                }catch{
+                    echo "No connections on $x"
+                    Write-Host
+                }
             }
-            }catch{
-                echo "No connections on $x"
-                Write-Host
+    }
+    if($Conn){
+        $Connections = Get-NetTCPConnection | ? RemoteAddress -eq $Conn
+        $Adapter = Get-CimInstance Win32_NetworkAdapterConfiguration | ? IPAddress -match $Connections.LocalAddress 
+        if($Adapter){
+            if($out){
+                echo "<----- $Adapter ----->">> $out
+                ft -InputObject $Adapter -Property Description, IPAddress, IPSubnet, DefaultIPGateway, DNSHostName, DNSServerSearchOrder -AutoSize >> $Out
+                echo "<----- Connections ----->">> $out
+            }else{
+                Write-Host "<----- $Adapter ----->" -ForegroundColor $GoodColor
+                ft -InputObject $Adapter -Property Description, IPAddress, IPSubnet, DefaultIPGateway, DNSHostName, DNSServerSearchOrder -AutoSize
+                Write-Host "<----- Connections ----->"
             }
+            foreach($x in $Connections){
+                if($out){
+                    ft -InputObject $Connections -Property LocalAddress, LocalPort, RemoteAddress, RemotePort, OwningProcess -AutoSize >> $Out
+                }else{
+                    ft -InputObject $Connections -Property LocalAddress, LocalPort, RemoteAddress, RemotePort, OwningProcess -AutoSize
+                }
+            }
+        }
+
     }
 
 
@@ -102,11 +127,17 @@ function Gather{
 }
 
 if($help){
-    Write-Host "Network Enumeration Tool"
-    Write-Host "Use -Verbose to get DNS Resolution and connection testing"
-    Write-Host "Use -Interface to specify a specific interface"
-    Write-Host "Use -Out to direct information to a specified file"
-    Write-Host "Use -Colorblind to display output in a more friendly color"
+    Write-Host "Simple network adapter analysis tool"
+    Write-Host 
+    Write-Host "NetEnum <command>"
+    Write-Host
+    write-host "Commands"
+    Write-Host "    -Verbose            Get DNS Resolution and connection testing."
+    Write-Host "    -Interface          Specify interface to analyze."
+    Write-Host "    -Conn               Search adapters by remote address connection."
+    Write-Host "    -Out                Direct output to file."
+    Write-Host "    -Colorblind         Friendly color options."
+    Write-Host
     exit
 }
 GlobalSettings
